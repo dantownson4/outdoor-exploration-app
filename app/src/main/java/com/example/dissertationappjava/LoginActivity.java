c8ea1254cc1e4b8b0e6885391632b7f7deb5fbe1
Townson, Daniel J <djt4@hw.ac.uk>
2022-01-28 18:09:09 +0000

    Reverted login template addition, implemented Google sign-in

---
+++ app/src/main/java/com/example/dissertationappjava/LoginActivity.java
@@ -0,0 +1,128 @@
+package com.example.dissertationappjava;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.dissertationappjava.databinding.ActivityLogin2Binding;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityLogin2Binding binding;
+    private int RC_SIGN_IN = 100;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build();
+        GoogleSignInClient gClient = GoogleSignIn.getClient(this, gso);
+
+        binding = ActivityLogin2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        /*setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_login);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                switch (view.getId()){
+                    case R.id.sign_in_button:
+                        userSignIn(gClient);
+                        break;
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_login);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        attemptStart(account);
+
+    }
+
+    private void userSignIn(GoogleSignInClient client){
+
+        Intent signInIntent = client.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resCode, Intent i) {
+
+        super.onActivityResult(reqCode, resCode, i);
+
+        if (reqCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(i);
+
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                attemptStart(account);
+            }catch (ApiException e){
+                Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+
+            }
+
+        }
+    }
+
+    private void attemptStart(GoogleSignInAccount account){
+
+        if (account != null){
+            Intent intent = new Intent(this, MainActivity.class);
+            finish();
+            startActivity(intent);
+        }
+    }
+}
